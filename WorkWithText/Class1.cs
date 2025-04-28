cd9dd84a1b54de4b8d8bd8b4ef657e98fd6d4eaa
unknown <@ngknn.local>
2025-04-28 14:36:59 +0300

    LongestWordInStringMethod

--- WorkWithText/Class1.cs
+++ WorkWithText/Class1.cs
@@ -1,7 +1,15 @@
 ﻿namespace WorkWithText
 {
-    public class Class1
+    public class TextUtilsLibrary
     {
+        public string GetLongestWord(string word)
+        {
+            if (string.IsNullOrWhiteSpace(word))
+                return string.Empty;
 
+            var words = word.Split(new[] { ' ', '\t', '\r', '\n', '.', ',', ';', ':', '!', '?', '-', '"' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return words.OrderByDescending(w => w.Length).FirstOrDefault() ?? string.Empty;
+        }
     }
 }
